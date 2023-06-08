10a30a38eb24932602159c5e8823255ce1814593
atmoner <contact.atmoner@gmail.com>
2023-06-08 23:33:51 +0200

    Add base in/out node

---
+++ src/nodes/base/InputNode.js
@@ -0,0 +1,17 @@
+import { defineNode, NodeInterface, TextInterface } from "baklavajs";
+
+export const InputNode = defineNode({
+  type: "InputNode",
+  title: "InputNode",
+  inputs: {    
+  },
+  outputs: {
+    value: () => new NodeInterface("Value", "")
+  },
+  calculate({ value }) {
+    return {
+      value: typeof value === "number" ? value.toFixed(3) : String(value)
+    };
+  }
+});
+ 
